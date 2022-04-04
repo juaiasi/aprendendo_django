ef7f5f9f6dd020947e511b345e352146eb913c9b
juimemorial <89988224+juimemorial@users.noreply.github.com>
2022-04-04 09:39:15 -0300

    templates formatados e com base.html sendo puxado

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views #manipula qual url queremos mostrar
 
 urlpatterns = [
-    path('', views.index, name='index') #puxa a visualização de views, com html
+    path('', views.index, name='index'), #puxa a visualização de views, com html
+    path('receita', views.receita, name='receita') #precisa estar na ordem para dar certo
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
     # return HttpResponse('<h1>Receitas</h1>')#não é ideal escrever o html aqui diretamente
+
+def receita(request):
+    return render(request,'receita.html')
