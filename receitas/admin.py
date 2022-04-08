c62810821404d2bdfda88cd5744ab079221dcc45
Juliana Iasi <juaiasi@gmail.com>
2022-04-08 09:25:09 -0300

    personalizando admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Receita
 
-admin.site.register(Receita)
+#mostra os campos no admin
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id','nome_receita','categoria')
+    list_display_link = ('id','nome_receita') #faz com que esses dois sejam links, não apenas o id
+    search_fields = ('nome_receita',) #campo de busca - precisa colocar uma virgula para entender que é uma tupla e não um elemento só
+    list_filter = ('categoria',) #categoria lateral para filtrar
+    list_per_page = 3 #faz ativar paginação a cada 3
+
+admin.site.register(Receita, ListandoReceitas) #se não incluir a segunda classe, o admin não vai ficar personalizado
